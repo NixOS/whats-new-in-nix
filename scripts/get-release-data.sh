d3075385c8360af526419b28c313954b7b5c5686
Dan Baker <daniel.n.baker@gmail.com>
2023-03-09 01:44:02 -0800

    Script to get release stats. (#2)

---
+++ scripts/get-release-data.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+set -Eeuo pipefail
+trap cleanup SIGINT SIGTERM ERR EXIT
+
+cleanup() {
+  trap - SIGINT SIGTERM ERR EXIT
+  # script cleanup here
+}
+
+allVersions=()
+for minorVersion in $(seq 7 14); do
+  allVersions+=("2.$minorVersion.0");
+done
+
+### Get aggregate stats
+printf "Version Range|# Commits|# Pull Requests|# Contributors\n" > release_stats.md
+
+# Double dot vs Triple Dot in git commands
+# https://stackoverflow.com/a/24186641/13358239
+for idx in $(seq 0 "$(( ${#allVersions[@]} - 2 ))"); do
+  prevVersion=${allVersions[$idx]}
+  currVersion=${allVersions[$idx + 1]}
+  range="$prevVersion - $currVersion"
+  commits=$(git rev-list --no-merges --count $prevVersion..$currVersion)
+  pullRequests=$(git log --oneline $prevVersion..$currVersion | grep 'Merge pull request #' | wc -l)
+  numContributors=$(git shortlog $prevVersion..$currVersion -e -n | rg ":$" | wc -l)
+  printf "$range|$commits|$pullRequests|$numContributors\n" >> release_stats.md
+done
+
+columnNames=$(head -1 release_stats.md | sed 's/|/,/g' | sed 's/ /_/g')
+column --separator "|" -td -N $columnNames -R $columnNames release_stats.md
+
+### Get contributor names
+for idx in $(seq 0 "$(( ${#allVersions[@]} - 2 ))"); do
+  contributors=$(git shortlog $prevVersion..$currVersion -e -n | rg ":$")
+  prevVersion=${allVersions[$idx]}
+  currVersion=${allVersions[$idx + 1]}
+  #echo "$contributors"
+  echo "$contributors" > "release_${prevVersion}_${currVersion}_contributors.md"
+
+done
